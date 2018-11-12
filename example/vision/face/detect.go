42034bfc75ec5bf5e080eccaf740fe03ab82fd1d
章建军 <zhangjianjun@zhangjianjundeMacBook-Pro.local>
2018-11-12 17:38:10 +0800

    use baidu face detect v3

--- example/vision/face/detect.go
+++ example/vision/face/detect.go
@@ -11,9 +11,9 @@ func DetectAndAnalysis() {
 	client := v3.NewFaceClient(APIKEY, APISECRET)
 	options := map[string]interface{}{
 		"max_face_num": 10,
-		"face_fields":  "age,beauty,expression,faceshape,gender,glasses,landmark,race,qualities",
-		"image_type": "BASE64",
-		"face_type": "LIVE",
+		"face_field":   "age,beauty,expression,face_shape,gender,glasses,landmark,race,quality,face_type",
+		"image_type":   "BASE64",
+		"face_type":    "LIVE",
 	}
 	rs, err := client.DetectAndAnalysis(
 		vision.MustFromFile("face.jpg"),
